08fac5dd56c6fa3a37eb507c8266933abbdc32fe
bookstore <6366129+BooookStore@users.noreply.github.com>
2020-11-26 20:37:23 +0900

    modify: 必要な依存関係を追加

--- spring-security-simple/build.gradle.kts
+++ spring-security-simple/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.1.4")
     implementation("mysql:mysql-connector-java:8.0.22")
 }
