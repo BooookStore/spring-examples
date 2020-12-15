45a5f2b0acb3b3ccc2edda3f1a352f46ccfa1676
bookstore <6366129+BooookStore@users.noreply.github.com>
2020-12-15 20:17:21 +0900

    modify: 基本情報の表示

--- spring-security-simple/src/main/java/com/example/spring/security/controller/AccountSettingController.java
+++ spring-security-simple/src/main/java/com/example/spring/security/controller/AccountSettingController.java
@@ -1,15 +1,38 @@
 package com.example.spring.security.controller;
 
+import com.example.spring.security.security.UserEntity;
+import com.example.spring.security.security.UserMapper;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/home/account")
 public class AccountSettingController {
 
+    private final UserMapper userMapper;
+
+    public AccountSettingController(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
     @GetMapping
-    public String account() {
+    @Transactional(readOnly = true)
+    public String account(Authentication authentication, Model model) {
+        Optional<UserEntity> optional = userMapper.findUserByUsername(authentication.getName());
+        UserEntity userEntity = optional.orElseThrow();
+
+        List<String> roles = userMapper.findRolesByUserId(userEntity.getId());
+
+        model.addAttribute("emailAddress", userEntity.getEmailAddress());
+        model.addAttribute("roles", String.join(", ", roles));
+
         return "account";
     }
 
