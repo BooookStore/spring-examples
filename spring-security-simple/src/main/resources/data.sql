2cf2a441b28e7395b79fc371abb1bf3f194d5522
bookstore <6366129+BooookStore@users.noreply.github.com>
2020-12-31 20:47:23 +0900

    modify: デフォルトユーザーを作成

---
+++ spring-security-simple/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (id, username, password, emailaddress)
+VALUES (1, 'bob', '12345', 'bob@sample.com');
+
+INSERT INTO roles (id, name)
+VALUES (1, 'ADMIN');
+
+INSERT INTO users (id, username, password, emailaddress)
+VALUES (2, 'alice', '12345', 'alice@sample.com');
+
+INSERT INTO roles (id, name)
+VALUES (2, 'NORMAL');
