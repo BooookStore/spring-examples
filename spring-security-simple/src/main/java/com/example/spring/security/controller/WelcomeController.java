f9f2145e2b8cd71e2db6ba4d5c2fbd375160069d
bookstore <6366129+BooookStore@users.noreply.github.com>
2021-01-04 12:06:06 +0900

    feat: ウェルカムページを作成

---
+++ spring-security-simple/src/main/java/com/example/spring/security/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.example.spring.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome() {
+        return "welcome";
+    }
+
+}
