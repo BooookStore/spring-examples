37cc4bbd5dbc7f72636e90e4a04ade9af76585b1
bookstore <6366129+BooookStore@users.noreply.github.com>
2021-01-04 18:32:10 +0900

    feat: ログアウトメッセージを表示

--- spring-security-simple/src/main/java/com/example/spring/security/controller/LoginController.java
+++ spring-security-simple/src/main/java/com/example/spring/security/controller/LoginController.java
@@ -1,13 +1,16 @@
 package com.example.spring.security.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("login")
-    public String login() {
+    public String login(@RequestParam(required = false) String logout, Model model) {
+        if (logout != null) model.addAttribute("logout", true);
         return "login";
     }
 
