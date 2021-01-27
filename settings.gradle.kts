51c586414acb35e51b401c6b8e61f2c84b09ed8a
bookstore <6366129+BooookStore@users.noreply.github.com>
2021-01-27 20:58:04 +0900

    add: プロジェクトを追加

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "spring-boot-docker-examples"
 include("simple-spring-boot-docker")
 include("spring-boot-docker-multi-stage-build")
 include("spring-security-simple")
+include("spring-security-global-method-security")
