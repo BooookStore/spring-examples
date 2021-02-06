2605ef2f76bb524c1d45f40e828f2b99458551c4
bookstore <6366129+BooookStore@users.noreply.github.com>
2021-02-06 22:30:34 +0900

    add: ユーザーモデルを作成

---
+++ spring-security-simple/src/main/java/com/example/spring/security/model/User.java
@@ -0,0 +1,50 @@
+package com.example.spring.security.model;
+
+public class User {
+
+    private int id;
+
+    private String username;
+
+    private String emailAddress;
+
+    public User(int id, String username, String emailAddress) {
+        this.id = id;
+        this.username = username;
+        this.emailAddress = emailAddress;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                '}';
+    }
+
+}
