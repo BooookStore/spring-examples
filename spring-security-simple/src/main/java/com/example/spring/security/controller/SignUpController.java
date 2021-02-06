6e8fa2c5c440558a0aabea06e62f468a7d6717de
bookstore <6366129+BooookStore@users.noreply.github.com>
2021-02-06 23:01:30 +0900

    fix: エンドポイントが保護されていない問題を修正

--- spring-security-simple/src/main/java/com/example/spring/security/controller/SignUpController.java
+++ spring-security-simple/src/main/java/com/example/spring/security/controller/SignUpController.java
@@ -1,7 +1,7 @@
 package com.example.spring.security.controller;
 
 import com.example.spring.security.security.UserEntity;
-import com.example.spring.security.security.UserMapper;
+import com.example.spring.security.security.SecurityUserMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,12 +20,12 @@ public class SignUpController {
 
     private final Logger logger = LoggerFactory.getLogger(SignUpController.class.getName());
 
-    private final UserMapper userMapper;
+    private final SecurityUserMapper securityUserMapper;
 
     private final PasswordEncoder passwordEncoder;
 
-    public SignUpController(UserMapper userMapper, PasswordEncoder passwordEncoder) {
-        this.userMapper = userMapper;
+    public SignUpController(SecurityUserMapper securityUserMapper, PasswordEncoder passwordEncoder) {
+        this.securityUserMapper = securityUserMapper;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -49,10 +49,10 @@ public class SignUpController {
 
     private void createNewUser(String username, String rawPassword) {
         String encodedPassword = passwordEncoder.encode(rawPassword);
-        int newIdentity = userMapper.generateNextIdentity();
+        int newIdentity = securityUserMapper.generateNextIdentity();
         UserEntity newUserEntity = new UserEntity(newIdentity, username, encodedPassword, null);
-        userMapper.insertUser(newUserEntity);
-        userMapper.insertRole(newIdentity, "NORMAL");
+        securityUserMapper.insertUser(newUserEntity);
+        securityUserMapper.insertRole(newIdentity, "NORMAL");
     }
 
 }
