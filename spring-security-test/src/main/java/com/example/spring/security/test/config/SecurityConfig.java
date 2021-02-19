70e3b910084712ee436656c0e77e0d50d54b246c
bookstore <6366129+BooookStore@users.noreply.github.com>
2021-02-19 21:35:34 +0900

    add: @WithUserDetails のテストを追加

--- spring-security-test/src/main/java/com/example/spring/security/test/config/SecurityConfig.java
+++ spring-security-test/src/main/java/com/example/spring/security/test/config/SecurityConfig.java
@@ -1,11 +1,33 @@
 package com.example.spring.security.test.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsManager = new InMemoryUserDetailsManager();
+        userDetailsManager.createUser(
+                User.withUsername("alice")
+                        .password("12345")
+                        .roles("admin")
+                        .build()
+        );
+        userDetailsManager.createUser(
+                User.withUsername("bob")
+                        .password("12345")
+                        .roles("staff")
+                        .build()
+        );
+        return userDetailsManager;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- spring-security-test/src/test/java/com/example/spring/security/test/AuthorizationTest.java
+++ spring-security-test/src/test/java/com/example/spring/security/test/AuthorizationTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -41,6 +42,20 @@ public class AuthorizationTest {
     @WithMockUser(roles = "staff")
     public void accessWithUserHasStaffRole() throws Exception {
         mockMvc.perform(get("/users"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithUserDetails("bob")
+    public void accessWithBobToNoPermitPath() throws Exception {
+       mockMvc.perform(get("/admin"))
+               .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithUserDetails("alice")
+    public void accessWithBobToPermitPath() throws Exception {
+        mockMvc.perform(get("/admin"))
                 .andExpect(status().isOk());
     }
 
