209add37b2ac546ea22fa68fa6c670827a045ff7
bookstore <6366129+BooookStore@users.noreply.github.com>
2021-02-27 12:18:42 +0900

    modify: 依存関係を更新

--- spring-security-oauth2-client/build.gradle.kts
+++ spring-security-oauth2-client/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity5")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
