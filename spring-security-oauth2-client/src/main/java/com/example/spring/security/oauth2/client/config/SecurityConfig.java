f3d92ec7e40e7f01ac58aee0b4075b1d4dcd9a2d
bookstore <6366129+BooookStore@users.noreply.github.com>
2021-03-07 13:15:59 +0900

    feat: デフォルトのログインページを home に変更

--- spring-security-oauth2-client/src/main/java/com/example/spring/security/oauth2/client/config/SecurityConfig.java
+++ spring-security-oauth2-client/src/main/java/com/example/spring/security/oauth2/client/config/SecurityConfig.java
@@ -28,7 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin();
+        http.formLogin()
+                .defaultSuccessUrl("/home", true);
         http.oauth2Client();
 
         http.authorizeRequests()
