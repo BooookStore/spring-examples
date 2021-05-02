d69c20040d0b23f0e7457e7fccf740d5797713ca
bookstore <6366129+BooookStore@users.noreply.github.com>
2021-05-02 15:48:58 +0900

    WIP: リソースサーバーに対するリクエストの送信
    * /home/request で WebClient を使用しリソースサーバーに対してリクエストを送信

--- spring-security-oauth2-client/src/main/java/com/example/spring/security/oauth2/client/controller/HomeController.java
+++ spring-security-oauth2-client/src/main/java/com/example/spring/security/oauth2/client/controller/HomeController.java
@@ -5,10 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 @Controller
 @RequestMapping("/home")
@@ -18,8 +22,11 @@ public class HomeController {
 
     private final OAuth2AuthorizedClientService authorizedClientService;
 
-    public HomeController(OAuth2AuthorizedClientService authorizedClientService) {
+    private final WebClient webClient;
+
+    public HomeController(OAuth2AuthorizedClientService authorizedClientService, WebClient webClient) {
         this.authorizedClientService = authorizedClientService;
+        this.webClient = webClient;
     }
 
     @GetMapping
@@ -33,6 +40,21 @@ public class HomeController {
         if (authorizedClient != null) {
             logger.info("access token value [{}]", authorizedClient.getAccessToken().getTokenValue());
         }
+
+        return "home";
+    }
+
+    @GetMapping("request")
+    public String requestToResourceServer(@RegisteredOAuth2AuthorizedClient("spring-oauth2-client-demo") OAuth2AuthorizedClient authorizedClient) {
+        String body = webClient
+                .get()
+                .uri("http://localhost:8082/getDocument")
+                .attributes(oauth2AuthorizedClient(authorizedClient))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        logger.info("retrieve from resource server [{}]", body);
 
         return "home";
     }
